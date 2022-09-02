4dee4729525388c4202671636eedf66d3e9ec6bc
Mufaqar Islam <mufaqar@gmail.com>
2022-09-02 10:40:41 +0500

    index

--- pages/works/index.js
+++ pages/works/index.js
@@ -1,22 +1,21 @@
-import WorkBox from "../../components/work-box";
+import WorkBox from '../../components/work-box';
 
 export default function index() {
-    return (
-        <>
-            <section className="custom-sec mt-48">
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                <WorkBox />
-                
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="custom-sec mt-48">
+        <WorkBox title="" image_path="" />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+        <WorkBox />
+      </section>
+    </>
+  );
 }
