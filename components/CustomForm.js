3a4ccd59b1eff23015bc9df595830f4f2b7175a9
Mufaqar Islam <mufaqar@gmail.com>
2022-10-04 20:02:27 +0500

    padding 30%

--- components/CustomForm.js
+++ components/CustomForm.js
@@ -2,54 +2,48 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import Logo from '../public/images/logo.png';
 
-export default function CustomForm({ status, message, onValidated, state, hideFields }) {
-
+export default function CustomForm({
+  status,
+  message,
+  onValidated,
+  state,
+  hideFields,
+}) {
   const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
   const [country, setCountry] = useState('');
   const [color, setColor] = useState(false);
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!email || !firstName || !country){
-      setColor(true)
-    }else{
+    if (!email || !firstName || !country) {
+      setColor(true);
+    } else {
       email &&
-      firstName &&
-      country &&
-      email.indexOf("@") > -1 &&
-      onValidated({
-        EMAIL: email,
-        MERGE1: firstName,
-        COUNTRY1: country,
-      });
+        firstName &&
+        country &&
+        email.indexOf('@') > -1 &&
+        onValidated({
+          EMAIL: email,
+          MERGE1: firstName,
+          COUNTRY1: country,
+        });
       state(false);
-    }   
-  }
-
-  
+    }
+  };
 
   return (
-    <form
-      className='mt-1'
-      onSubmit={(e) => handleSubmit(e)}
-    >
-
-      {
-      status === "sending" && (
-        <div className="mc__alert mc__alert--sending">
-          sending...
-        </div>
-      )
-      }
-      {status === "error" && (
+    <form className="mt-1" onSubmit={(e) => handleSubmit(e)}>
+      {status === 'sending' && (
+        <div className="mc__alert mc__alert--sending">sending...</div>
+      )}
+      {status === 'error' && (
         <div
           className="mc__alert mc__alert--error"
           dangerouslySetInnerHTML={{ __html: message }}
         />
       )}
-      {status === "success" && (
+      {status === 'success' && (
         <div className="font-FoundersGroteskRegular flex items-center lg:text-[21px]">
           <p>Thank You</p>
           <figure className="mt-3">
@@ -59,7 +53,6 @@ export default function CustomForm({ status, message, onValidated, state, hideFi
       )}
 
       <div className={hideFields ? 'block' : 'hidden'}>
-
         <div className="flex items-center justify-between mt-1">
           <input
             onChange={(e) => setFirstName(e.target.value)}
@@ -69,7 +62,13 @@ export default function CustomForm({ status, message, onValidated, state, hideFi
             placeholder="Name"
             isRequired
           />
-          <p className={firstName ? 'hidden' : `block text-right ${color ? 'text-red-500' : 'text-black'}`}>
+          <p
+            className={
+              firstName
+                ? 'hidden'
+                : `block text-right ${color ? 'text-red-500' : 'text-black'}`
+            }
+          >
             Required Field
           </p>
         </div>
@@ -83,7 +82,13 @@ export default function CustomForm({ status, message, onValidated, state, hideFi
             placeholder="Email"
             isRequired
           />
-          <p className={email ? 'hidden' : `block text-right ${color ? 'text-red-500' : 'text-black'}`}>
+          <p
+            className={
+              email
+                ? 'hidden'
+                : `block text-right ${color ? 'text-red-500' : 'text-black'}`
+            }
+          >
             Required Field
           </p>
         </div>
@@ -97,24 +102,27 @@ export default function CustomForm({ status, message, onValidated, state, hideFi
             placeholder="Country"
             isRequired
           />
-          <p className={country ? 'hidden' : `block text-right ${color ? 'text-red-500' : 'text-black'}`}>
+          <p
+            className={
+              country
+                ? 'hidden'
+                : `block text-right ${color ? 'text-red-500' : 'text-black'}`
+            }
+          >
             Enter an email address
           </p>
         </div>
-
       </div>
 
       <input
         label="subscribe"
         type="submit"
         value="Enter"
-        className={`text-lg cursor-pointer md:text-xl input_field ${hideFields ? 'block' : 'hidden'}`}
+        className={`text-lg cursor-pointer md:text-xl input_field ${
+          hideFields ? 'block' : 'hidden'
+        }`}
         formValues={[email, firstName, country]}
       />
-
     </form>
   );
-};
-
-
-
+}
--- components/work-box.js
+++ components/work-box.js
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import OwnImage from './OwnImage';
 
-export default function WorkBox({title, imageURL, imageCaption, slug}) {
+export default function WorkBox({ title, imageURL, imageCaption, slug }) {
   return (
     <>
       <div className="mt-[60px] mb-2">
         <div>
           <Link href={`/works/${slug}`}>
-            <figure className="cursor-pointer md:pr-[30%]">
-              <OwnImage url={imageURL} alt={imageCaption}/>
+            <figure className="cursor-pointer md:pr-[25%]">
+              <OwnImage url={imageURL} alt={imageCaption} />
             </figure>
           </Link>
           <h2 className="font-FoundersGroteskMedium cursor-pointer lg:text-[65px] md:text-[51px] text-[42px] leading-[42px] md:leading-[77px]">
