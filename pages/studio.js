73103e8ce5b4cb644130322fc839721302981edc
syed-zunair-gillani <gillanix007@gmail.com>
2022-11-17 14:06:17 +0500

    update

--- pages/studio.js
+++ pages/studio.js
@@ -1,16 +1,16 @@
-import Image from 'next/image';
-import Book from '../components/book';
-import Wallpeper from '../public/images/wallpeper.png';
-import Gray from '../public/images/GRAY.png';
-import Deezen from '../public/images/DEEZEN.png';
-import TheGlobe from '../public/images/THEGLOBE.png';
-import Canadian from '../public/images/CANADIAN.png';
-import Team from '../components/team';
-import { useState } from 'react';
-import { AiOutlineCaretDown } from 'react-icons/ai';
-import { AiOutlineCaretUp } from 'react-icons/ai';
-import MblTeam from '../components/mblTeam';
-import { sanityClient } from '../lib/sanity';
+import Image from "next/image";
+import Book from "../components/book";
+import Wallpeper from "../public/images/wallpeper.png";
+import Gray from "../public/images/GRAY.png";
+import Deezen from "../public/images/DEEZEN.png";
+import TheGlobe from "../public/images/THEGLOBE.png";
+import Canadian from "../public/images/CANADIAN.png";
+import Team from "../components/team";
+import { useState } from "react";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { AiOutlineCaretUp } from "react-icons/ai";
+import MblTeam from "../components/mblTeam";
+import { sanityClient } from "../lib/sanity";
 // import video from '../public/video.mp4'
 
 // sanity Queries
@@ -61,8 +61,6 @@ export default function Studio({
   publications,
   books,
 }) {
-
-
   const [openTeamDetails, setOpenTeamDetails] = useState(null); // To handel Team Section
   const [pressActive, setPressActive] = useState(false);
   const [countUpdate, setCountUpdate] = useState(null);
@@ -79,9 +77,9 @@ export default function Studio({
     setCountUpdate(id);
   };
 
-  const TeamDetailClick=()=>{
+  const TeamDetailClick = () => {
     setOpenTeamDetails(null);
-  }
+  };
 
   return (
     <>
@@ -120,7 +118,7 @@ export default function Studio({
           </div>
         </div>
         <div
-          className={`${pressActive ? 'block sm:block' : 'hidden sm:block'}`}
+          className={`${pressActive ? "block sm:block" : "hidden sm:block"}`}
         >
           <div className="Press-sec">
             <div>
@@ -160,65 +158,120 @@ export default function Studio({
                 Exhibitions and Talks
               </h2>
             </div>
-            <div className="grid gap-[13px] mt-8 eandt lg:grid-cols-5 md:grid-cols-2">
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2019</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  ARCHITECTURE AND DESIGN NOW TALK
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Lethbridge University, AB
-                </p>
-              </div>
+            <div className="grid gap-[13px] mt-8 lg:grid-cols-5 md:grid-cols-2">
+              <section>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px]">2019</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    ARCHITECTURE AND DESIGN NOW TALK
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Lethbridge University, AB
+                  </p>
+                </div>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px] mt-[13px]">2017</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    MoV ECO CITIES
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Museum of Vancouver, BC
+                  </p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    URBANARIUM
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Robson Square, Vancouver, BC
+                  </p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    GRAY MAGAZINE PANEL
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Venue, Seattle, WA, United States of America
+                  </p>
+                </div>
+              </section>
 
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2016</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  YOUR FUTURE HOME: CREATING THE NEW VANCOUVER
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Exhibition Name, Museum of Vancouver, BC
-                </p>
-              </div>
+              <section>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px]">2016</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    YOUR FUTURE HOME: CREATING THE NEW VANCOUVER
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Exhibition Name, Museum of Vancouver, BC
+                  </p>
+                </div>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px] mt-[13px]">2014</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    THE WEST COAST MODERN HOUSE BOOK
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Ocean Park, Wallace Street House
+                  </p>
+                </div>
+              </section>
 
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2014</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  THE WEST COAST MODERN HOUSE BOOK
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Ocean Park, Wallace Street House
-                </p>
-              </div>
+              <section>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px]">2012</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    PECHA KULCHA TALK
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Venue, City of Richmond Culture Days, BC
+                  </p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    PECHA KULCHA 21 TALK
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Vogue Theatre, Vancouver, BC
+                  </p>
+                </div>
 
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2010</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  20+ CHANGE EXHIBITION
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Dalhousie University of Architecture, Halifax, NS
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Emily Carr University of Art and Design Vancouver, BC
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  AIBC Architecture Centre Gallery, Vancouver, BC
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  La Maison de l’architecture du Quebec, Montreal, QC
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  The Gladstone Hotel, Toronto, ON
-                </p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  HIGH PERFORMANCE EXHIBITION
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Emily Carr University of Art and Design Vancouver, BC
-                </p>
-              </div>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px] mt-[13px]">2011</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    VIEW FROM THE EDGE EXHIBITION
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Architectural Institue of British Columbia, Vancouver, BC
+                  </p>
+                </div>
+              </section>
 
+              <section>
+                <div>
+                  <p className="font-FoundersGroteskMedium text-[22px]">2010</p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    20+ CHANGE EXHIBITION
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Dalhousie University of Architecture, Halifax, NS
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Emily Carr University of Art and Design Vancouver, BC
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    AIBC Architecture Centre Gallery, Vancouver, BC
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    La Maison de l’architecture du Quebec, Montreal, QC
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    The Gladstone Hotel, Toronto, ON
+                  </p>
+                  <p className="font-FoundersGroteskMedium text-[18px]">
+                    HIGH PERFORMANCE EXHIBITION
+                  </p>
+                  <p className="font-FoundersGroteskRegular text-[17px]">
+                    Emily Carr University of Art and Design Vancouver, BC
+                  </p>
+                </div>
+              </section>
+
+              <section>
               <div>
                 <p className="font-FoundersGroteskMedium text-[22px]">2009</p>
                 <p className="font-FoundersGroteskMedium text-[18px]">
@@ -230,57 +283,7 @@ export default function Studio({
               </div>
 
               <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2017</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  MoV ECO CITIES
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Museum of Vancouver, BC
-                </p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  URBANARIUM
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Robson Square, Vancouver, BC
-                </p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  GRAY MAGAZINE PANEL
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Venue, Seattle, WA, United States of America
-                </p>
-              </div>
-
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2012</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  PECHA KULCHA TALK
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Venue, City of Richmond Culture Days, BC
-                </p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  PECHA KULCHA 21 TALK
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Vogue Theatre, Vancouver, BC
-                </p>
-              </div>
-
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2011</p>
-                <p className="font-FoundersGroteskMedium text-[18px]">
-                  VIEW FROM THE EDGE EXHIBITION
-                </p>
-                <p className="font-FoundersGroteskRegular text-[17px]">
-                  Architectural Institue of British Columbia, Vancouver, BC
-                </p>
-              </div>
-
-              <div></div>
-
-              <div>
-                <p className="font-FoundersGroteskMedium text-[22px]">2004</p>
+                <p className="font-FoundersGroteskMedium text-[22px] mt-[13px]">2004</p>
                 <p className="font-FoundersGroteskMedium text-[18px]">
                   MAKING OUT EXHIBITION
                 </p>
@@ -288,6 +291,8 @@ export default function Studio({
                   Emily Carr University of Art and Design Vancouver, BC
                 </p>
               </div>
+              </section>
+
             </div>
           </div>
         </div>
@@ -310,7 +315,7 @@ export default function Studio({
               <div
                 key={index}
                 className={`relative ${
-                  openTeamDetails === item._id && 'col-start-1 row-num'
+                  openTeamDetails === item._id && "col-start-1 row-num"
                 } `}
               >
                 <div
@@ -320,10 +325,10 @@ export default function Studio({
                   <Team src={item.profileimage.asset.url} />
                 </div>
                 <div
-                  onClick={()=>TeamDetailClick()}
+                  onClick={() => TeamDetailClick()}
                   className={`${
-                    openTeamDetails === item._id ? 'block' : 'hidden'
-                  } bg-black overflow-y-scroll -top-[26px] absolute h-full lg:left-[102.5%] w-screen  z-10 md:left-[103%] text-white custom-sec py-7`}
+                    openTeamDetails === item._id ? "block" : "hidden"
+                  } bg-black overflow-y-scroll -top-[42px] absolute h-full lg:left-[102.5%] w-screen  z-10 md:left-[103%] text-white custom-sec py-7`}
                 >
                   <div className="flex flex-col px-5 lg:flex-row lg:gap-12">
                     <div className="lg:w-[200px] w-full flex flex-col justify-between mb-3">
